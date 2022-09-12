b90353dc38ece0e260448273ce82a74059ccd3b9
Mojib <mujibenishi2014@gmail.com>
2022-09-12 20:20:06 +1000

    version1

---
+++ CovidState/script.js
@@ -0,0 +1,25 @@
+//Getting Data from Covid Api using Ajax 
+$.ajax({
+    type: 'get',
+    url: 'https://api.covid19api.com/summary',
+    success: function (response){
+        
+        for(var i = 0; i < response.Countries.length; i++){
+            var totalactive = response.Countries[i].TotalConfirmed-response.Countries[i].TotalRecovered
+            
+            var tablerow = `<tr><td>${response.Countries[i].Country}</td>
+                                <td>${response.Countries[i].TotalConfirmed}</td>
+                                <td>${totalactive}</td>
+                                <td>${response.Countries[i].TotalRecovered}</td>
+                                <td>${response.Countries[i].TotalDeaths}</td>
+            </tr>`
+            $('#tbody').append(tablerow);
+        }
+        $('#covidtable').DataTable();
+        
+    },
+    error: function (error){
+        alert(error);
+    }
+})
+//End of Ajax Function 
